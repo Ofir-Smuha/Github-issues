ce1d1703cbcf5090c0b01451bcdfbb825e014005
Ofir Smuha <ofir@500tech.com>
2018-11-08 08:12:31 +0200

    PR comments resolved

--- src/reducers/issues.reducer.js
+++ src/reducers/issues.reducer.js
@@ -1,6 +1,8 @@
 import { set } from 'lodash/fp';
 import { handleActions } from 'redux-actions';
 
+import { SET_ISSUES, SET_CURRENT_ISSUE } from 'actions/issues.actions';
+
 // import type BaseAction from 'types/redux.types'
 import type { Issue, Issues } from 'components/issues/issues.types';
 
@@ -16,9 +18,9 @@ export type IssuesState = {|
 
 export default handleActions(
   {
-    SET_ISSUES: (state: IssuesState, { payload }): IssuesState =>
+    [SET_ISSUES]: (state: IssuesState, { payload }): IssuesState =>
       set('openIssues', payload.openIssues, state),
-    SET_CURRENT_ISSUE: (state: IssuesState, { payload }) => {
+    [SET_CURRENT_ISSUE]: (state: IssuesState, { payload }) => {
       const currentIssue = state.openIssues.find(
         issue => issue.id.toString() === payload.issueId
       );
