7c55a73d91da829f75657c07469230a95a3fcc34
Ofir Smuha <ofir@500tech.com>
2018-12-03 08:31:09 +0200

    cleaned

--- src/middlewares/api.middleware.js
+++ src/middlewares/api.middleware.js
@@ -49,10 +49,6 @@ const apiMiddleware: Middleware = ({ dispatch, getState }) => {
     apiUtils
       .request({ method, url: path, data, headers })
       .then(({ body, header, status }) => {
-        console.log('PR check');
-
-        console.log('PR check');
-        console.log('PR check');
         if (handleHeaders) {
           dispatchActions(handleHeaders, header);
         }
