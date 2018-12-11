f0033697749084a1ecaf14c03b07bd73ef4fd14f
Ofir Smuha <ofir@500tech.com>
2018-12-11 10:16:00 +0200

    NewIssue / FilterAddBar cmps added

---
+++ src/components/issues/FillterAddBar.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class FillterAddBar extends Component {
+  render() {
+    return <div />;
+  }
+}
+
+export default FillterAddBar;
---
+++ src/components/issues/NewIssue.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class NewIssue extends Component {
+  render() {
+    return <div />;
+  }
+}
+
+export default NewIssue;
